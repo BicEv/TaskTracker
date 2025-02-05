1769604b36b4936d21b1c853bac9fe15ab3f83e8
BicEv <bickbovev@gmail.com>
2025-02-05 20:34:21 +0300

    UserRestController

---
+++ src/main/java/ru/bicev/TaskTracker/controller/UserRestCotroller.java
@@ -0,0 +1,67 @@
+package ru.bicev.TaskTracker.controller;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import ru.bicev.TaskTracker.dto.UserDto;
+import ru.bicev.TaskTracker.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserRestCotroller {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserRestCotroller(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
+        UserDto createdUser = userService.createUser(userDto);
+        return new ResponseEntity<UserDto>(createdUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{userId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long userId) {
+        UserDto user = userService.getUserById(userId);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @GetMapping("/{username}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UserDto> getUserByUsername(@RequestParam String username) {
+        UserDto user = userService.getUserByUsername(username);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable Long userId, @Valid @RequestBody UserDto userDto,
+            Principal principal) {
+        UserDto updatedUser = userService.updateUser(userId, userDto, principal);
+        return ResponseEntity.ok().body(updatedUser);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userId, Principal principal) {
+        userService.deleteUser(userId, principal);
+        return ResponseEntity.noContent().build();
+    }
+
+}
